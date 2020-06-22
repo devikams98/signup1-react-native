462aba4dbfc7c004a3be0dd8c703aec46c2395ec
devikams98 <msdevika101198@gmail.com>
2020-06-22 17:41:31 +0530

    sample signup

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Regform from './app/components/Regform';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Regform />
+      
+        </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#4d5250',
+    paddingLeft: 60,
+    paddingRight: 60,
+    
+  },
+});
